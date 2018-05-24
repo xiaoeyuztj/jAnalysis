87ca795b4a9d3c0abd62f8dc72fa9589971ae173
Jing Li <j.li@cern.ch>
2018-05-24 22:07:09 +0200

    version

---
+++ jROOT/__version__.py
@@ -0,0 +1,9 @@
+__title__ = "jROOT"
+__description__ = (
+        'A collection of utility functions dealing with Minitree files',
+        'for the ATLAS Experiment at CERN.'
+        )
+__url__ = 'https://github.com/XD-Jing/jAnalysis'
+__version__ = '0.1'
+__author__ = "Jing Li"
+__author_email__ = "j.li@cern.ch"
